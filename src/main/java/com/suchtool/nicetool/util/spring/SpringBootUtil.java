2a231af68125db3ba78d38b48ca0711f91d30478
knifeopen <2716552863@qq.com>
2026-03-30 09:52:28 +0800

    1.1.36版本发布（添加项目路径分析工具）

--- src/main/java/com/suchtool/nicetool/util/spring/SpringBootUtil.java
+++ src/main/java/com/suchtool/nicetool/util/spring/SpringBootUtil.java
@@ -2,13 +2,32 @@ package com.suchtool.nicetool.util.spring;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.lang.reflect.Method;
 import java.util.Map;
 
 public class SpringBootUtil {
+    /**
+     * 解析运行类的包名
+     */
     public static String parseRunClassPackage() {
         Map<String, Object> runClassBeanMap = ApplicationContextHolder.getContext()
                 .getBeansWithAnnotation(SpringBootApplication.class);
         Object runObject = runClassBeanMap.entrySet().iterator().next().getValue();
         return AopUtil.getTargetClass(runObject).getPackage().getName();
+    }
+
+    /**
+     * 判断方法是否在运行类的包下边
+     */
+    public static boolean inRunClassPackage(Method method) {
+        return inRunClassPackage(method.getDeclaringClass());
+    }
+
+    /**
+     * 判断类是否在运行类的包下边
+     */
+    public static boolean inRunClassPackage(Class<?> declaringClass) {
+        String runPackageName = SpringBootUtil.parseRunClassPackage();
+        return declaringClass.getName().startsWith(runPackageName.trim());
     }
 }
