9f7ca433ace93bfa5d17473e9677bb27e5112bd2
knifeopen <2716552863@qq.com>
2026-06-22 11:29:20 +0800

    1.1.38版本发布（优化文件工具）

--- src/main/java/com/suchtool/nicetool/util/file/FileUtil.java
+++ src/main/java/com/suchtool/nicetool/util/file/FileUtil.java
@@ -1,13 +1,15 @@
 package com.suchtool.nicetool.util.file;
 
+import com.suchtool.nicetool.util.file.vo.FileNameExtVO;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.fileupload.FileItem;
-import org.apache.commons.fileupload.FileItemFactory;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.io.IOUtils;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.util.Assert;
 import org.springframework.util.StreamUtils;
+import org.springframework.util.StringUtils;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.multipart.MultipartFile;
@@ -15,11 +17,19 @@ import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
-import java.io.*;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 
 @Slf4j
 public class FileUtil {
+
+    private static final String DEFAULT_FIELD_NAME = "file";
+
+    private static final int DISK_FILE_ITEM_FACTORY_SIZE_THRESHOLD = 10240;
 
     /**
      * 字节输入流转换为包含二进制数据+文件名称的MultipartFile文件
@@ -27,61 +37,75 @@ public class FileUtil {
      * @param inputStream 字节输入流
      * @param fileName    文件名
      */
-    public static MultipartFile inputStreamToMultipartFile(InputStream inputStream, String fileName) throws IOException {
-        // 该方法可用于测试：return new MockMultipartFile(fileName, fileName, MediaType.MULTIPART_FORM_DATA_VALUE, inputStream);
+    public static MultipartFile toMultipartFile(InputStream inputStream, String fileName) {
         FileItem fileItem = createFileItem(inputStream, fileName);
-        // CommonsMultipartFile是feign对multipartFile的封装，但是要FileItem类对象
+        if (fileItem == null) {
+            throw new RuntimeException("创建FileItem失败");
+        }
         return new CommonsMultipartFile(fileItem);
+    }
+
+    /**
+     * 解析文件名和扩展名，无扩展名时扩展名为空字符串
+     *
+     * @param fileName 完整文件名
+     */
+    public static FileNameExtVO parseFileNameAndExt(String fileName) {
+        if (!StringUtils.hasText(fileName)) {
+            throw new RuntimeException("文件名不能为空");
+        }
+
+        String tmpFileName = "";
+        String tmpExtName = "";
+        int index = fileName.lastIndexOf('.');
+        if (index < 0) {
+            tmpFileName = StringUtils.trimWhitespace(fileName);
+        } else if (index == 0) {
+            tmpFileName = "";
+            tmpExtName = StringUtils.trimWhitespace(fileName.substring(1));
+        } else {
+            tmpFileName = StringUtils.trimWhitespace(fileName.substring(0, index));
+            tmpExtName = StringUtils.trimWhitespace(fileName.substring(index + 1));
+        }
+
+        FileNameExtVO fileNameExtVO = new FileNameExtVO();
+        fileNameExtVO.setFileName(tmpFileName);
+        fileNameExtVO.setExtName(tmpExtName);
+        return fileNameExtVO;
     }
 
     /**
      * FileItem类对象创建
      *
-     * @param inputStream inputStream
-     * @param fileName    fileName
+     * @param inputStream 输入流
+     * @param fileName    文件名
      * @return FileItem
      */
     public static FileItem createFileItem(InputStream inputStream, String fileName) {
-        FileItemFactory factory = new DiskFileItemFactory(16, null);
-        String textFieldName = fileName;
-        FileItem item = factory.createItem(textFieldName, MediaType.MULTIPART_FORM_DATA_VALUE, true, fileName);
-        int bytesRead = 0;
-        byte[] buffer = new byte[10 * 1024 * 1024];
-        OutputStream os = null;
-        // 使用输出流输出输入流的字节
+        return createFileItem(inputStream, DEFAULT_FIELD_NAME, fileName);
+    }
+
+    private static FileItem createFileItem(InputStream inputStream, String fieldName, String fileName) {
+        DiskFileItemFactory factory = new DiskFileItemFactory(DISK_FILE_ITEM_FACTORY_SIZE_THRESHOLD, null);
+        FileItem fileItem = factory.createItem(fieldName, MediaType.MULTIPART_FORM_DATA_VALUE, true, fileName);
+        OutputStream outputStream = null;
         try {
-            os = item.getOutputStream();
-            while ((bytesRead = inputStream.read(buffer, 0, 8192)) != -1) {
-                os.write(buffer, 0, bytesRead);
-            }
-            inputStream.close();
+            outputStream = fileItem.getOutputStream();
+            IOUtils.copy(inputStream, outputStream);
         } catch (IOException e) {
-            throw new RuntimeException("文件上传失败", e);
+            throw new RuntimeException("写入FileItem异常", e);
         } finally {
-            if (os != null) {
-                try {
-                    os.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-
-                }
-            }
-            if (inputStream != null) {
-                try {
-                    inputStream.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
+            IOUtils.closeQuietly(outputStream);
+            IOUtils.closeQuietly(inputStream);
         }
-
-        return item;
+        return fileItem;
     }
 
     /**
      * 将文件导出到响应
+     *
      * @param fileContent 文件内容
-     * @param fileName 文件名
+     * @param fileName    文件名
      */
     public static void exportFileToResponse(byte[] fileContent, String fileName) {
         OutputStream outputStream = assembleResponseOutputStream(fileName);
@@ -94,11 +118,12 @@ public class FileUtil {
     }
 
     /**
-     * 组装响应输出流
+     * 组装响应
+     *
      * @param fileName 文件名
      * @return 响应输出流
      */
-    public static ServletOutputStream assembleResponseOutputStream(String fileName) {
+    public static HttpServletResponse assembleResponse(String fileName) {
         ServletRequestAttributes servletRequestAttributes =
                 (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
         Assert.notNull(servletRequestAttributes, "RequestAttributes不能为null");
@@ -106,21 +131,29 @@ public class FileUtil {
         HttpServletResponse response = servletRequestAttributes.getResponse();
         Assert.notNull(response, "response不能为null");
 
-        String tmpFileName = null;
+        String tmpFileName;
         try {
             tmpFileName = URLEncoder.encode(fileName, "UTF-8");
         } catch (UnsupportedEncodingException e) {
             throw new RuntimeException(e);
         }
 
-        // 设置响应头信息
         response.setContentType(MediaType.MULTIPART_FORM_DATA_VALUE);
         response.setHeader(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=" + tmpFileName);
         response.setCharacterEncoding("utf-8");
-        // response.setHeader("traceId", NiceLogTraceIdUtil.readTraceId());
 
+        return response;
+    }
+
+    /**
+     * 组装响应输出流
+     *
+     * @param fileName 文件名
+     * @return 响应输出流
+     */
+    public static ServletOutputStream assembleResponseOutputStream(String fileName) {
         try {
-            return response.getOutputStream();
+            return assembleResponse(fileName).getOutputStream();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
